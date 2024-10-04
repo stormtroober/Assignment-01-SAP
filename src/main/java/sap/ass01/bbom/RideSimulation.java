7f38082a0c91e863bee813fe8deda7ab0b75779c
Alessandro Ricci <a.ricci@unibo.it>
2024-10-04 10:58:37 +0200

    Fixed bug in AddEBikeDialog

--- src/main/java/sap/ass01/bbom/RideSimulation.java
+++ src/main/java/sap/ass01/bbom/RideSimulation.java
@@ -37,9 +37,19 @@ public class RideSimulation extends Thread {
 			l = b.getLocation();
 			if (l.x() > 200 || l.x() < -200) {
 				b.updateDirection(new V2d(-d.x(), d.y()));
+				if (l.x() > 200) {
+					b.updateLocation(new P2d(200, l.y()));
+				} else {
+					b.updateLocation(new P2d(-200, l.y()));
+				}
 			};
 			if (l.y() > 200 || l.y() < -200) {
 				b.updateDirection(new V2d(d.x(), -d.y()));
+				if (l.y() > 200) {
+					b.updateLocation(new P2d(l.x(), 200));
+				} else {
+					b.updateLocation(new P2d(l.x(), -200));
+				}
 			};
 			
 			/* change dir randomly */
