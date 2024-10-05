940a505775764497395ad8b370de9a27eef85604
Tonelli Luca <luca.tonelli11@studio.unibo.it>
2024-10-05 19:01:26 +0200

    :white_check_mark: test: add architecture test

---
+++ app/src/test/java/org/model/ArchitectureTest.java
@@ -0,0 +1,24 @@
+package org.model;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
+
+
+@AnalyzeClasses(packages = "sap.ass01.layered")
+public class ArchitectureTest {
+
+    @ArchTest
+    public static final ArchRule layer_dependencies_are_respected = layeredArchitecture()
+            .consideringAllDependencies()
+            .layer("Presentation").definedBy("sap.ass01.layered.UI..")
+            .layer("Business").definedBy("sap.ass01.layered.domain..", "sap.ass01.layered.services..")
+            .layer("Persistence").definedBy("sap.ass01.layered.persistence..")
+            .whereLayer("Presentation").mayNotBeAccessedByAnyLayer()
+            .whereLayer("Business").mayOnlyBeAccessedByLayers("Presentation")
+            .whereLayer("Persistence").mayOnlyBeAccessedByLayers("Business");
+
+
+}
