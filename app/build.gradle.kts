90e2ca8c1a1a0274a4b6e48473889d39cedffc24
Alessandro Becci <alessandro.becci@studio.unibo.it>
2024-10-09 13:39:11 +0200

    chores: updated build gradle to have mysql connector

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,13 +7,14 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
-    testImplementation("com.tngtech.archunit:archunit-junit5:1.3.0")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("mysql:mysql-connector-java:8.0.33")
     implementation("com.google.guava:guava:32.0.1-jre")
     implementation("org.slf4j:slf4j-simple:1.7.32")
     implementation("com.google.code.gson:gson:2.11.0")
     implementation("org.mongodb:mongodb-driver-sync:4.4.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    testImplementation("com.tngtech.archunit:archunit-junit5:1.3.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 configurations.all {
