9a22f5d0275ebba2b0b063a4771c32215d7cc214
Tonelli Luca <luca.tonelli11@studio.unibo.it>
2024-10-09 18:48:35 +0200

    :recycle: refactor: change DTO file in persistent package form class to record

--- app/src/main/java/sap/ass01/layered/persistence/dto/UserDTO.java
+++ app/src/main/java/sap/ass01/layered/persistence/dto/UserDTO.java
@@ -1,26 +1,5 @@
 package sap.ass01.layered.persistence.dto;
 
-public class UserDTO {
-    private final String id;
-    private final int credit;
-    private final String permission;
-
-    public UserDTO(final String id, final int credit, final String permission) {
-        this.id = id;
-        this.credit = credit;
-        this.permission = permission;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public int getCredit() {
-        return credit;
-    }
-
-    public String getPermission() {
-        return permission;
-    }
+public record UserDTO(String id, int credit, String permission) {
 }
 
