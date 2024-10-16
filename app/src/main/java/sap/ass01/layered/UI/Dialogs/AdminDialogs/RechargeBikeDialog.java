68d001ecd1a306768bf8fed21015b47365be1af6
Ludovico Nollino <s1093732@studenti.univpm.it>
2024-10-16 18:42:59 +0200

    feat: completed the implementation of RechargeBikeDialog and RechargeCreditDialog

--- app/src/main/java/sap/ass01/layered/UI/Dialogs/AdminDialogs/RechargeBikeDialog.java
+++ app/src/main/java/sap/ass01/layered/UI/Dialogs/AdminDialogs/RechargeBikeDialog.java
@@ -1,6 +1,7 @@
 package sap.ass01.layered.UI.Dialogs.AdminDialogs;
 
 import sap.ass01.layered.UI.Dialogs.AbstractDialog;
+import sap.ass01.layered.services.Services.AdminService;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -8,15 +9,17 @@ import java.awt.event.ActionEvent;
 public class RechargeBikeDialog extends AbstractDialog {
 
     private JTextField idField;
+    private final AdminService adminService;
 
-    public RechargeBikeDialog(JFrame parent) {
+    public RechargeBikeDialog(JFrame parent, AdminService adminService) {
         super(parent, "Recharge Bike");
+        this.adminService = adminService;
         setupDialog();
     }
 
     private void setupDialog() {
         idField = new JTextField();
-        addField("E-Bike ID:", idField);
+        addField("E-Bike ID to recharge:", idField);
     }
 
     @Override
@@ -25,6 +28,18 @@ public class RechargeBikeDialog extends AbstractDialog {
         if (e.getSource() == confirmButton) {
             String id = idField.getText();
             // Handle the recharge bike logic here
+            /*adminService.rechargeEBike(id)
+                    .subscribe(
+                            ebikeDTO -> {
+                                // Handle success
+                                JOptionPane.showMessageDialog(this, "E-Bike recharged successfully: " + ebikeDTO);
+                                dispose();
+                            },
+                            throwable -> {
+                                // Handle error
+                                JOptionPane.showMessageDialog(this, "Error recharging E-Bike: " + throwable.getMessage());
+                            }
+                    );*/
             dispose();
         }
     }
