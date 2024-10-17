dd80f74527077c0f53fae3ed36b607dcdd581be8
Alessandro Becci <trofui@gmail.com>
2024-10-17 13:13:13 +0200

    feat: added support for plugin to color the bikes based on the state

--- app/src/main/java/sap/ass01/layered/ui/views/AdminView.java
+++ app/src/main/java/sap/ass01/layered/ui/views/AdminView.java
@@ -1,5 +1,8 @@
+// File: AdminView.java
 package sap.ass01.layered.ui.views;
 
+import sap.ass01.layered.plugin.ColorStatePlugin;
+import sap.ass01.layered.plugin.PluginClassLoader;
 import sap.ass01.layered.ui.dialogs.admin.AddEBikeDialog;
 import sap.ass01.layered.ui.dialogs.admin.RechargeBikeDialog;
 import sap.ass01.layered.ui.models.UserViewModel;
@@ -11,18 +14,22 @@ import sap.ass01.layered.ui.models.EBikeViewModel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
 import java.util.Collection;
 import java.util.List;
 
 public class AdminView extends AbstractView {
 
     private final AdminService adminService = ServiceFactory.getAdminService();
+    private ColorStatePlugin plugin;
 
     public AdminView(UserViewModel user) {
         super("Admin View", user);
         setupView();
         observeAllBikes();
         updateVisualizerPanel();
+        //registerNewEffectPlugin("ColorStateEffect", new File("plugins/ColorStateEffect.jar"));
+        //testPluginOnBikeInstance();
     }
 
     private void setupView() {
@@ -54,8 +61,6 @@ public class AdminView extends AbstractView {
                 );
     }
 
-
-
     private void updateAllBikes(Collection<EBikeDTO> allBikes) {
         // Update the UI components with the new available bikes data
         List<EBikeViewModel> bikesModel = allBikes.stream()
@@ -67,13 +72,41 @@ public class AdminView extends AbstractView {
         refreshView();
     }
 
-
-
     public void refreshView() {
         updateVisualizerPanel();
     }
 
     private void log(String msg){
         System.out.println("[AdminView] "+msg);
+    }
+
+    private void registerNewEffectPlugin(String pluginID, File libFile)  {
+        try{
+            var loader = new PluginClassLoader(libFile.getAbsolutePath());
+            String className = "sap.ass01.layered.effects.ColorStateEffect";
+            Class<?> pluginClass = loader.loadClass(className);
+            plugin = (ColorStatePlugin) pluginClass.getDeclaredConstructor(null).newInstance(null);
+            log("Added plugin-in " + pluginID);
+        } catch (Exception e) {
+            log("Error loading plugin: " + e.getMessage());
+        }
+    }
+
+    public EBikeViewModel applyEffect(EBikeViewModel bike) {
+        EBikeViewModel updatedBike = bike;
+        if (plugin != null) {
+            try{
+                updatedBike = plugin.colorState(bike);
+            } catch (Exception e) {
+                log("Error applying plugin: " + e.getMessage());
+            }
+        }
+        return updatedBike;
+    }
+
+    private void testPluginOnBikeInstance() {
+        EBikeViewModel bike = new EBikeViewModel("bike1", 10.0, 20.0, 80, "active");
+        log("Before applying plugin: " + bike);
+        log("After applying plugin: " + applyEffect(bike));
     }
 }
